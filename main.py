fe74cefd4eaac12252b42127d80d31d05bbadcba
jacobrees <jacob.rees97@yahoo.com>
2020-09-19 09:33:21 +0100

    add point market to map

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import folium
 
 map = folium.Map(location=[51.5, -0.1], zoom_start=6)
+map.add_child(folium.Marker(location=[51.622846, -3.941989], popup="shitty city", icon=folium.Icon(color="green")))
 map.save("Map1.html")
